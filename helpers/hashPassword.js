424896f47aaae8cc132d8a6da78799b031967f29
Nurfiah Idris <nurfiahidris@Nurfiahs-MacBook-Air.local>
2020-07-09 22:33:08 +0800

    jwt-adding

--- helpers/hashPassword.js
+++ helpers/hashPassword.js
@@ -2,14 +2,18 @@ const bcrypt = require(`bcryptjs`);
 const salt = bcrypt.genSaltSync(8);
 
 function hashingPassword(password) {
-    const hash = bcrypt.hashSync('password', salt);
+    const hash = bcrypt.hashSync(password, salt);
     return hash
 }
 
+
 function comparePassword(password, hash) {
+
     const result = bcrypt.compareSync(password, hash)
     return result;
 }
+
+// console.log(comparePassword("12345", "$2a$08$jIreL4ZUWDHIp72.0SoJael8NGdX82GJsMMbnwieckdoG2VBAEDI."));
 
 module.exports = {
     hashingPassword,
