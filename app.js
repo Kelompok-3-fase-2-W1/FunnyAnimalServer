a58c7d4c516f8182d40ccde2e34784455d84c990
Daniel Soerjonoto <soerjonoto.daniel@gmail.com>
2020-07-10 13:24:31 +0700

    add app.use(cors())

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const router = require('./routers/index.js')
-const cors = require('cors')
+const cors = require('cors');
+app.use(cors())
 
 const PORT = process.env.port;
 
